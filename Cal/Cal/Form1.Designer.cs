426cb67b2466c0a4ee0b39337cd0af5e4b246665
LnwBaimon <suvarnabhumi29@gmail.com>
2025-10-19 19:28:43 +0700

    Calculator app program

---
+++ Cal/Cal/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Cal
+{
+    partial class Calculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            num_1 = new Button();
+            num_2 = new Button();
+            num_3 = new Button();
+            num_4 = new Button();
+            num_5 = new Button();
+            num_6 = new Button();
+            num_7 = new Button();
+            num_8 = new Button();
+            num_9 = new Button();
+            num_0 = new Button();
+            Clear = new Button();
+            cal = new Button();
+            display = new TextBox();
+            Div = new Button();
+            Mul = new Button();
+            Sub = new Button();
+            Add = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // num_1
+            // 
+            num_1.BackColor = SystemColors.ControlDark;
+            num_1.FlatAppearance.BorderSize = 0;
+            num_1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            num_1.ForeColor = SystemColors.ControlLightLight;
+            num_1.Location = new Point(52, 137);
+            num_1.Name = "num_1";
+            num_1.Size = new Size(69, 46);
+            num_1.TabIndex = 0;
+            num_1.Text = "1";
+            num_1.UseVisualStyleBackColor = false;
+            num_1.Click += number_Click;
+            // 
+            // num_2
+            // 
+            num_2.BackColor = SystemColors.ControlDark;
+            num_2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            num_2.ForeColor = SystemColors.ControlLightLight;
+            num_2.Location = new Point(154, 137);
+            num_2.Name = "num_2";
+            num_2.Size = new Size(69, 46);
+            num_2.TabIndex = 1;
+            num_2.Text = "2";
+            num_2.UseVisualStyleBackColor = false;
+            num_2.Click += number_Click;
+            // 
+            // num_3
+            // 
+            num_3.BackColor = SystemColors.ControlDark;
+            num_3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            num_3.ForeColor = SystemColors.ControlLightLight;
+            num_3.Location = new Point(260, 137);
+            num_3.Name = "num_3";
+            num_3.Size = new Size(69, 46);
+            num_3.TabIndex = 2;
+            num_3.Text = "3";
+            num_3.UseVisualStyleBackColor = false;
+            num_3.Click += number_Click;
+            // 
+            // num_4
+            // 
+            num_4.BackColor = SystemColors.ControlDark;
+            num_4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            num_4.ForeColor = SystemColors.ControlLightLight;
+            num_4.Location = new Point(52, 201);
+            num_4.Name = "num_4";
+            num_4.Size = new Size(69, 46);
+            num_4.TabIndex = 3;
+            num_4.Text = "4";
+            num_4.UseVisualStyleBackColor = false;
+            num_4.Click += number_Click;
+            // 
+            // num_5
+            // 
+            num_5.BackColor = SystemColors.ControlDark;
+            num_5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            num_5.ForeColor = SystemColors.ControlLightLight;
+            num_5.Location = new Point(154, 201);
+            num_5.Name = "num_5";
+            num_5.Size = new Size(69, 46);
+            num_5.TabIndex = 4;
+            num_5.Text = "5";
+            num_5.UseVisualStyleBackColor = false;
+            num_5.Click += number_Click;
+            // 
+            // num_6
+            // 
+            num_6.BackColor = SystemColors.ControlDark;
+            num_6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            num_6.ForeColor = SystemColors.ControlLightLight;
+            num_6.Location = new Point(260, 201);
+            num_6.Name = "num_6";
+            num_6.Size = new Size(69, 46);
+            num_6.TabIndex = 5;
+            num_6.Text = "6";
+            num_6.UseVisualStyleBackColor = false;
+            num_6.Click += number_Click;
+            // 
+            // num_7
+            // 
+            num_7.BackColor = SystemColors.ControlDark;
+            num_7.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            num_7.ForeColor = SystemColors.ControlLightLight;
+            num_7.Location = new Point(52, 265);
+            num_7.Name = "num_7";
+            num_7.Size = new Size(69, 46);
+            num_7.TabIndex = 6;
+            num_7.Text = "7";
+            num_7.UseVisualStyleBackColor = false;
+            num_7.Click += number_Click;
+            // 
+            // num_8
+            // 
+            num_8.BackColor = SystemColors.ControlDark;
+            num_8.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            num_8.ForeColor = SystemColors.ControlLightLight;
+            num_8.Location = new Point(154, 265);
+            num_8.Name = "num_8";
+            num_8.Size = new Size(69, 46);
+            num_8.TabIndex = 7;
+            num_8.Text = "8";
+            num_8.UseVisualStyleBackColor = false;
+            num_8.Click += number_Click;
+            // 
+            // num_9
+            // 
+            num_9.BackColor = SystemColors.ControlDark;
+            num_9.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            num_9.ForeColor = SystemColors.ControlLightLight;
+            num_9.Location = new Point(260, 265);
+            num_9.Name = "num_9";
+            num_9.Size = new Size(69, 46);
+            num_9.TabIndex = 8;
+            num_9.Text = "9";
+            num_9.UseVisualStyleBackColor = false;
+            num_9.Click += number_Click;
+            // 
+            // num_0
+            // 
+            num_0.BackColor = SystemColors.ControlDark;
+            num_0.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            num_0.ForeColor = SystemColors.ControlLightLight;
+            num_0.Location = new Point(154, 328);
+            num_0.Name = "num_0";
+            num_0.Size = new Size(69, 46);
+            num_0.TabIndex = 9;
+            num_0.Text = "0";
+            num_0.UseVisualStyleBackColor = false;
+            num_0.Click += number_Click;
+            // 
+            // Clear
+            // 
+            Clear.BackColor = SystemColors.ControlLight;
+            Clear.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Clear.ForeColor = SystemColors.ControlText;
+            Clear.Location = new Point(52, 328);
+            Clear.Name = "Clear";
+            Clear.Size = new Size(69, 46);
+            Clear.TabIndex = 10;
+            Clear.Text = "C";
+            Clear.UseVisualStyleBackColor = false;
+            Clear.Click += btnClear_Click;
+            // 
+            // cal
+            // 
+            cal.BackColor = Color.Gold;
+            cal.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cal.ForeColor = SystemColors.ControlLightLight;
+            cal.Location = new Point(260, 328);
+            cal.Name = "cal";
+            cal.Size = new Size(69, 46);
+            cal.TabIndex = 11;
+            cal.Text = "=";
+            cal.UseVisualStyleBackColor = false;
+            cal.Click += btnEqual_Click;
+            // 
+            // display
+            // 
+            display.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            display.Location = new Point(95, 52);
+            display.Name = "display";
+            display.Size = new Size(300, 51);
+            display.TabIndex = 12;
+            display.TextChanged += display_TextChanged;
+            // 
+            // Div
+            // 
+            Div.BackColor = Color.Gold;
+            Div.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Div.ForeColor = SystemColors.ControlLightLight;
+            Div.Location = new Point(360, 137);
+            Div.Name = "Div";
+            Div.Size = new Size(69, 46);
+            Div.TabIndex = 13;
+            Div.Text = "/";
+            Div.UseVisualStyleBackColor = false;
+            Div.Click += operator_Click;
+            // 
+            // Mul
+            // 
+            Mul.BackColor = Color.Gold;
+            Mul.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Mul.ForeColor = SystemColors.ControlLightLight;
+            Mul.Location = new Point(360, 201);
+            Mul.Name = "Mul";
+            Mul.Size = new Size(69, 46);
+            Mul.TabIndex = 14;
+            Mul.Text = "*";
+            Mul.UseVisualStyleBackColor = false;
+            Mul.Click += operator_Click;
+            // 
+            // Sub
+            // 
+            Sub.BackColor = Color.Gold;
+            Sub.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Sub.ForeColor = SystemColors.ControlLightLight;
+            Sub.Location = new Point(360, 265);
+            Sub.Name = "Sub";
+            Sub.Size = new Size(69, 46);
+            Sub.TabIndex = 15;
+            Sub.Text = "-";
+            Sub.UseVisualStyleBackColor = false;
+            Sub.Click += operator_Click;
+            // 
+            // Add
+            // 
+            Add.BackColor = Color.Gold;
+            Add.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Add.ForeColor = SystemColors.ControlLightLight;
+            Add.Location = new Point(360, 328);
+            Add.Name = "Add";
+            Add.Size = new Size(69, 46);
+            Add.TabIndex = 16;
+            Add.Text = "+";
+            Add.UseVisualStyleBackColor = false;
+            Add.Click += operator_Click;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(22, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(240, 30);
+            label1.TabIndex = 17;
+            label1.Text = "Calculator  :)";
+            // 
+            // Calculator
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(476, 450);
+            Controls.Add(label1);
+            Controls.Add(Add);
+            Controls.Add(Sub);
+            Controls.Add(Mul);
+            Controls.Add(Div);
+            Controls.Add(display);
+            Controls.Add(cal);
+            Controls.Add(Clear);
+            Controls.Add(num_0);
+            Controls.Add(num_9);
+            Controls.Add(num_8);
+            Controls.Add(num_7);
+            Controls.Add(num_6);
+            Controls.Add(num_5);
+            Controls.Add(num_4);
+            Controls.Add(num_3);
+            Controls.Add(num_2);
+            Controls.Add(num_1);
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Name = "Calculator";
+            Text = "Calculator App";
+            Load += Calculator_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button num_1;
+        private Button num_2;
+        private Button num_3;
+        private Button num_4;
+        private Button num_5;
+        private Button num_6;
+        private Button num_7;
+        private Button num_8;
+        private Button num_9;
+        private Button num_0;
+        private Button Clear;
+        private Button cal;
+        private TextBox display;
+        private Button Div;
+        private Button Mul;
+        private Button Sub;
+        private Button Add;
+        private Label label1;
+    }
+}
---
+++ Cal/Cal/Form1.cs
@@ -0,0 +1,82 @@
+﻿namespace Cal
+{
+    public partial class Calculator : Form
+    {
+        double value = 0;
+        string operation = "";
+        bool operationPressed = false;
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void num_1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void display_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void number_Click(object sender, EventArgs e)
+        {
+            if ((display.Text == "0") || operationPressed)
+                display.Clear();
+
+            operationPressed = false;
+            Button b = (Button)sender;
+            display.Text += b.Text;
+        }
+
+        private void operator_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            operation = b.Text;
+            value = double.Parse(display.Text);
+            operationPressed = true;
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            double secondValue = double.Parse(display.Text);
+            switch (operation)
+            {
+                case "+": display.Text = (value + secondValue).ToString(); break;
+                case "-": display.Text = (value - secondValue).ToString(); break;
+                case "*": display.Text = (value * secondValue).ToString(); break;
+                case "/":
+                    display.Text = secondValue != 0 ?
+                        (value / secondValue).ToString() : "Error";
+                    break;
+            }
+
+            operationPressed = false;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            display.Text = "0";
+            value = 0;
+            operation = "";
+        }
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is Button)
+                {
+                    c.Cursor = Cursors.Hand;
+                }
+            }
+        }
+    }
+}
